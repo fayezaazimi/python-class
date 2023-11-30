21eea2128fd2569d4bd8e8ca83006c20122066e6
fayezaazimi <152495378+fayezaazimi@users.noreply.github.com>
2023-11-30 15:01:24 +0330

    Add files via upload

---
+++ test2.2.py
@@ -0,0 +1,11 @@
+ca = input("enter ca(+, -,*,/):")
+a = int(input("enter a: "))
+b = int(input("enter b:"))
+if ca == "+":
+    print(a+b)
+if ca =="-":
+    print(a-b)
+if ca =="*":
+    print(a*b)    
+if ca =="/":
+    print(a/b)
---
+++ test2.3.py
@@ -0,0 +1,7 @@
+a=int (input("enter a:"))
+b=int (input("enter b:"))
+c=int (input("enter c:"))
+if a+b>c and a+c>b and c+b>a:
+    print("true")
+else:
+    print("false")    
